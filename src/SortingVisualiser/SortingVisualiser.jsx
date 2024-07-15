69cb22efedfa9aa44dc49a4dda33459eb8ad6003
ThomBarros <thomas.b.barros@gmail.com>
2024-07-16 00:03:58 +1000

    First attempt at implementing quick sort. Animations/ algo is broken

--- src/SortingVisualiser/SortingVisualiser.jsx
+++ src/SortingVisualiser/SortingVisualiser.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {getMergeSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
+import {getMergeSortAnimations, getQuickSortAnimations} from '../sortingAlgorithms/sortingAlgorithms.js';
 import './SortingVisualiser.css';
 
 // Change this value for the speed of the animations.
@@ -38,29 +38,49 @@ export default class SortingVisualizer extends React.Component {
     mergeSort() {
         const animations = getMergeSortAnimations(this.state.array);
         for (let i = 0; i < animations.length; i++) {
-        const arrayBars = document.getElementsByClassName('array-bar');
-        const isColorChange = i % 3 !== 2;
-        if (isColorChange) {
-            const [barOneIdx, barTwoIdx] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-            setTimeout(() => {
-                barOneStyle.backgroundColor = color;
-                barTwoStyle.backgroundColor = color;
-            }, i * ANIMATION_SPEED_MS);
-        } else {
-            setTimeout(() => {
-                const [barOneIdx, newHeight] = animations[i];
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
                 const barOneStyle = arrayBars[barOneIdx].style;
-                barOneStyle.height = `${newHeight}px`;
-            }, i * ANIMATION_SPEED_MS);
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
         }
     }
-}
 
     quickSort() {
-    // We leave it as an exercise to the viewer of this code to implement this method.
+        const animations = getQuickSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
     }
 
     heapSort() {
--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -76,4 +76,84 @@ function doMerge(
     }
 }
 
+export function getQuickSortAnimations(array) {
+    const animations = [];
+    if (array.length <= 1) return array;
+    quickSort(array, 0, (array.length - 1), animations);
+    return animations;
+}
 
+function quickSort(
+    mainArray,
+    startIdx,
+    endIdx,
+    animations,
+) {
+    // Implementation of quick sort using Lomuto partition scheme with "fat pivot"
+    if (startIdx >= 0 && startIdx < endIdx) {
+        const [lesser, greater] = quickSortPartition(mainArray, startIdx, endIdx, animations);
+        quickSort(mainArray, startIdx, (lesser - 1), animations);
+        quickSort(mainArray, (greater + 1), endIdx, animations);
+    }
+}
+
+function quickSortPartition(
+    mainArray,
+    startIdx,
+    endIdx,
+    animations,
+) {
+    let pivot = mainArray[Math.floor((startIdx + endIdx) / 2)];
+    let lesser = startIdx;
+    let equal = startIdx;
+    let greater = endIdx;
+
+    while (equal <= greater) {
+        if (mainArray[equal] < pivot) {
+            // These are the values that we're comparing; we push them once
+            // to change their color.
+            animations.push([equal, pivot]);
+            // These are the values that we're comparing; we push them a second
+            // time to revert their color.
+            animations.push([equal, pivot]);
+
+            swapElements(mainArray, equal, lesser);
+
+            animations.push([equal, lesser]); 
+            // move the bars
+
+            lesser = lesser + 1;
+            equal = equal + 1;
+        } else if (mainArray[equal] > pivot) {
+            // These are the values that we're comparing; we push them once
+            // to change their color.
+            animations.push([equal, pivot]);
+            // These are the values that we're comparing; we push them a second
+            // time to revert their color.
+            animations.push([equal, pivot]);
+
+            swapElements(mainArray, equal, greater);
+
+            animations.push([equal, greater]) 
+            // move the bars
+
+            greater = greater - 1;
+        } else { // if mainArray[equal] == pivot
+            // These are the values that we're comparing; we push them once
+            // to change their color.
+            animations.push([equal, pivot]);
+            // These are the values that we're comparing; we push them a second
+            // time to revert their color.
+            animations.push([equal, pivot]);
+
+            animations.push([equal, pivot])
+
+            equal = equal + 1;
+        }
+    }
+    return [lesser, greater];
+}
+
+function swapElements (array, index1, index2) {
+    array[index1] = array.splice(index2, 1, array[index1])[0];
+}
